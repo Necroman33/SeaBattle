2a2f742f11b00ba3b32ef0218de4ef45b27fe7fb
Necroman33 <57381978+Necroman33@users.noreply.github.com>
2021-06-28 09:26:29 +0300

    Доработка всего в целом

---
+++ SeaBattle/Bot.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace SeaBattle
+{
+    public class Bot
+    {
+        public int[,] myMap = new int[10, 10];
+        public int[,] enemyMap = new int[10, 10];
+
+        public Button[,] myButtons = new Button[10, 10];
+        public Button[,] enemyButtons = new Button[10, 10];
+
+        public Bot(int[,] myMap, int[,] enemyMap, Button[,] myButtons, Button[,] enemyButtons)
+        {
+            this.myMap = myMap;
+            this.enemyMap = enemyMap;
+            this.enemyButtons = enemyButtons;
+            this.myButtons = myButtons;
+        }
+
+
+
+        public bool IsInsideMap(int i, int j)
+        {
+            if (i < 0 || j < 0 || i >= 10 || j >= 10)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public bool IsEmpty(int i, int j, int length)
+        {
+            bool isEmpty = true;
+
+            for (int k = j; k < j + length; k++)
+            {
+                if (myMap[i, k] != 0)
+                {
+                    isEmpty = false;
+                    break;
+                }
+            }
+
+            return isEmpty;
+        }
+
+        public int[,] ConfigureShips()
+        {
+            int lengthShip = 4;
+            int cycleValue = 4;
+            int shipsCount = 10;
+            Random r = new Random();
+
+            int posX = 0;
+            int posY = 0;
+
+            while (shipsCount > 0)
+            {
+                for (int i = 0; i < cycleValue / 4; i++)
+                {
+                    posX = r.Next(0, 10);
+                    posY = r.Next(0, 10);
+
+                    while (!IsInsideMap(posX, posY + lengthShip - 1) || !IsEmpty(posX, posY, lengthShip))
+                    {
+                        posX = r.Next(0, 10);
+                        posY = r.Next(0, 10);
+                    }
+                    for (int k = posY; k < posY + lengthShip; k++)
+                    {
+                        myMap[posX, k] = 1;
+                    }
+
+
+
+                    shipsCount--;
+                    if (shipsCount <= 0)
+                        break;
+                }
+                cycleValue += 4;
+                lengthShip--;
+            }
+            return myMap;
+        }
+
+
+        public bool Shoot()
+        {
+            bool hit = false;
+            Random r = new Random();
+
+            int posX = r.Next(0, 10);
+            int posY = r.Next(0, 10);
+
+            while (enemyButtons[posX, posY].Background == Brushes.Red || enemyButtons[posX, posY].Background == Brushes.Blue)
+            {
+                posX = r.Next(0, 10);
+                posY = r.Next(0, 10);
+            }
+
+            if (enemyMap[posX, posY] != 0)
+            {
+                hit = true;
+                enemyMap[posX, posY] = 3;
+                enemyButtons[posX, posY].Background = Brushes.Red;
+            }
+            else
+            {
+                hit = false;
+                enemyButtons[posX, posY].Background = Brushes.Blue;
+            }
+            if (hit)
+                Shoot();
+            return hit;
+        }
+
+    }
+}
--- SeaBattle/Game.xaml.cs
+++ SeaBattle/Game.xaml.cs
@@ -22,16 +22,34 @@ namespace SeaBattle
     {
         SeaBattleEntities context = new SeaBattleEntities();
         Logic logic = new Logic();
+        public int[,] myMap = new int[10, 10];
+        public int[,] enemyMap = new int[10, 10];
+        public bool isPlaying = false;
+        public Button[,] myButtons = new Button[10, 10];
+        public Button[,] enemyButtons = new Button[10, 10];
+        public Bot bot;
         public Game(string user)
         {
             InitializeComponent();
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    myMap[i, j] = 0;
+                    enemyMap[i, j] = 0;
+                }
+            }
             username_label.Content += user;
-            addEventClicCell(playerShips);
-            addEventClicCell(enemyShips);
+            addEventClicCell(playerShips,1);
+            addEventClicCell(enemyShips,2);
+            isPlaying = false;
+            bot = new Bot(enemyMap, myMap, enemyButtons, myButtons);
+            enemyMap = bot.ConfigureShips();
+
         }
 
 
-        private void addEventClicCell(Grid grid)
+        private void addEventClicCell(Grid grid, int type)
         {
             int i = 0;
             int j = 0;
@@ -43,9 +61,24 @@ namespace SeaBattle
                     j = 0;
                 }
                 Button b = new Button();
-                b.Click += B_Click;
+                if(type == 1)
+                {
+                    b.Click += B_Click;
+                }
+                else
+                {
+                    b.Click += PlayerShoot;
+                }
                 b.Tag = new Point(i, j);
                 grid_item.Children.Add(b);
+                if(type == 1)
+                {
+                    myButtons[i, j] = b;
+                }
+                else
+                {
+                    enemyButtons[i, j] = b;
+                }
                 j++;
             }
         }
@@ -54,25 +87,17 @@ namespace SeaBattle
         {
             Button button = sender as Button;
             Point point = (Point)button.Tag;
-            if (button.Background == Brushes.Red)
+            if (!isPlaying)
             {
-                button.Background = Brushes.Yellow;
-            }
-            else {
-                if (button.Background == Brushes.Yellow)
+                if (myMap[(int)point.X, (int)point.Y] == 0)
                 {
-                    button.Background = Brushes.Blue;
+                    button.Background = Brushes.Green;
+                    myMap[(int)point.X, (int)point.Y] = 1;
                 }
                 else
                 {
-                    if (button.Background == Brushes.Blue)
-                    {
-                        button.Background = null;
-                    }
-                    else
-                    {
-                        button.Background = Brushes.Red;
-                    }
+                    button.Background = null;
+                    myMap[(int)point.X, (int)point.Y] = 0;
                 }
             }
         }
@@ -94,5 +119,140 @@ namespace SeaBattle
             }
         }
 
+        public int CheckIfMapIsNotEmpty()
+        {
+            int result = 0;
+            int test = 0;
+            int test2 = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    if (myMap[i, j] != 0&& myMap[i, j] != 3)
+                        test = 1;
+                    if (enemyMap[i, j] != 0 && myMap[i, j] != 3)
+                        test2 = 1;
+                }
+            }
+            if(test == 0)
+            {
+                result = 1;
+            }
+            else
+            {
+                if (test2 == 0)
+                {
+                    result = 2;
+                }
+            }
+            return result;
+        }
+
+        private void startButton_Click(object sender, RoutedEventArgs e)
+        {
+            isPlaying = true;
+        }
+
+        public void restart()
+        {
+            isPlaying = false;
+            for (int i = 0; i < 10; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+
+                    myMap[i, j] = 0;
+                    enemyMap[i, j] = 0;
+                }
+            }
+            addEventClicCell(playerShips, 1);
+            addEventClicCell(enemyShips, 2);
+            enemyMap = bot.ConfigureShips();
+
+        }
+
+        public void PlayerShoot(object sender, EventArgs e)
+        {
+            var scores = context.Scores.ToList();
+            var user_scores = scores.Where(u => u.Username == (string)username_label.Content).FirstOrDefault();
+            if (isPlaying)
+            {
+                Button pressedButton = sender as Button;
+                bool playerTurn = Shoot(enemyMap, pressedButton);
+                if (!playerTurn)
+                    bot.Shoot();
+                if (CheckIfMapIsNotEmpty() == 1)
+                {
+                    MessageBox.Show("Вы проиграли!");
+                    restart();
+                    if ((string)username_label.Content != "Неизвестный")
+                    {
+                        user_scores.Defeats++;
+                        context.SaveChanges();
+                    }
+                }
+                else
+                {
+                    if (CheckIfMapIsNotEmpty() == 2)
+                    {
+                        MessageBox.Show("Вы победили!");
+                        restart();
+                        if ((string)username_label.Content != "Неизвестный")
+                        {
+                            user_scores.Wins++;
+                            context.SaveChanges();
+                        }
+                    }
+                }
+            }
+        }
+      /* public void Destroyed(Button b)
+        {
+            Button button = b;
+            Point point = (Point)button.Tag;
+            int status = 0;
+            if (enemyMap[])
+            {
+
+            }
+            enemyButtons[(int)point.X-1,(int)point.Y-1].Background = Brushes.Red
+        }*/
+       
+        public bool Shoot(int[,] map, Button pressedButton)
+        {
+            Point point = (Point)pressedButton.Tag;
+            bool hit = false;
+            if (isPlaying)
+            {
+                if (map[(int)point.X, (int)point.Y] != 0)
+                {
+                    hit = true;
+                    map[(int)point.X, (int)point.Y] = 3;
+                    pressedButton.Background = Brushes.Red;
+                }
+                else
+                {
+                    hit = false;
+
+                    pressedButton.Background = Brushes.Blue;
+                }
+            }
+            return hit;
+        }
+
+        private void give_up_Click(object sender, RoutedEventArgs e)
+        {
+            if (isPlaying) { 
+                var scores = context.Scores.ToList();
+                var user_scores = scores.Where(u => u.Username == (string)username_label.Content).FirstOrDefault();
+                MessageBox.Show("Вы проиграли!");
+                restart();
+            if ((string)username_label.Content != "Неизвестный")
+            {
+                user_scores.Defeats++;
+                context.SaveChanges();
+            }
+            }
+        }
     }
 }
